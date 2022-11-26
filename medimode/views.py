af157e1f8888dc54be8a47fbe668cb0b6b89a155
Yash-Keswani <yash20158@iiitd.ac.in>
2022-11-26 23:04:53 +0530

    fixed search

--- medimode/views.py
+++ medimode/views.py
@@ -458,6 +458,7 @@ class TicketView(AuthDetailView):
 		
 with open("medimode/models/cities.json") as _fl:
 	state_text = _fl.read()
+	
 class Search(AuthTemplateView):
 	template_name = "medimode/search.html"
 	
@@ -473,13 +474,21 @@ class Search(AuthTemplateView):
 		location_state = get_clean(request.POST, "state")
 		location_city = get_clean(request.POST, "city")
 		
-		all_objs = category.objects.filter(Q(location_state=location_state) & Q(location_city=location_city))
-		names_obj = {x.full_name: x.user for x in all_objs}
+		if not (request.POST.get("loc_search") or request.POST.get("name_search") or request.POST.get("search")):
+			raise ValidationError("Invalid Search category")
+			
+		all_objs = category.objects.all()
+		if request.POST.get("loc_search") or request.POST.get("search"):
+			all_objs = category.objects.filter(Q(location_state=location_state) & Q(location_city=location_city))
+			entries = category.objects.all().select_related("user")
 		
-		entries_close = set([names_obj[x] for x in difflib.get_close_matches(entity_name, names_obj.keys(), n=10)])
-		entries_including = set([names_obj[x] for x in names_obj if entity_name in x])
+		if request.POST.get("name_search") or request.POST.get("search"):
+			names_obj = {x.full_name: x.user for x in all_objs}
+			
+			entries_close = set([names_obj[x] for x in difflib.get_close_matches(entity_name, names_obj.keys(), n=10)])
+			entries_including = set([names_obj[x] for x in names_obj if entity_name in x])
 		
-		entries = list(entries_close | entries_including)
-		ctx = {"entries": entries, "state_json": state_text, "state_dict": json.loads(state_text)}
-		
-		return render(request, self.template_name, context=ctx)
+			entries = list(entries_close | entries_including)
+			
+		ctx = {"entries": [{"role": x.role, "name": x.profile.full_name, "bio": x.profile.bio, "id": x.profile.id} for x in entries]}
+		return JsonResponse(data=ctx)
