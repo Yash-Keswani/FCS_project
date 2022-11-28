7c6037af33e37d326c7aff3b83de85b9a6be0ad2
Yashasvi Chaurasia <yashasvi12977@gmail.com>
2022-11-28 15:05:16 +0000

    fixed vkeyerror
    Signed-off-by: Yashasvi Chaurasia <yashasvi12977@gmail.com>

--- static/otp.js
+++ static/otp.js
@@ -1,9 +1,9 @@
 function input(e) {
-    var keyInput = document.getElementById("keyInput");
+    var keyInput = document.getElementById("id_otp");
     keyInput.value = keyInput.value + e;
 }
 
 function deleting_val() {
-    var tbInput = document.getElementById("keyInput");
+    var tbInput = document.getElementById("id_otp");
     tbInput.value = tbInput.value.substr(0, tbInput.value.length - 1);
 }
