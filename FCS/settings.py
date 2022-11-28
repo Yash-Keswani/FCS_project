a61da60a7125f9ffefbf580300d8471c7b71c09c
Yash-Keswani <yash20158@iiitd.ac.in>
2022-11-28 20:20:01 +0530

    truncated database added new db settings

--- FCS/settings.py
+++ FCS/settings.py
@@ -70,12 +70,15 @@ WSGI_APPLICATION = 'FCS.wsgi.application'
 }
 """
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': BASE_DIR / 'db.sqlite3',
-	}
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dontfuckwiththefirewall',
+        'USER': 'hari',
+        'PASSWORD': 'keswanigods#999',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
